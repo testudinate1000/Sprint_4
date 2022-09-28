c45b9f53159b3b5b49e478c2701f8eb586d54084
testudinate1000 <olenakuznietsova@yandex.ru>
2022-09-28 16:54:06 +0300

    Transfer the method from header_page to main_page

--- page_object/header.py
+++ page_object/header.py
@@ -6,7 +6,6 @@ class HeaderPage:
     scooter_logo = (By.CLASS_NAME, 'Header_LogoScooter__3lsAR')
     yandex_logo = (By.CLASS_NAME, 'Header_LogoYandex__3TSOI')
     order_button_header = (By.XPATH, './/div[@class = "Header_Nav__AGCXC"]/button[text()="Заказать"]')
-    cookie_button = (By.XPATH, './/button[text()="да все привыкли"]')
 
     def __init__(self, driver):
         self.driver = driver
@@ -30,7 +29,3 @@ class HeaderPage:
     def click_header_order_button(self):
         self.driver.find_element(*self.order_button_header).click()
 
-    # метод кликает по кнопке «да все привыкли»
-    @allure.step('Принять coockie')
-    def click_cookie_button(self):
-        self.driver.find_element(*self.cookie_button).click()
