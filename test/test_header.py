0b3a9a51276df83914769fc0906bfc6e70380ab7
testudinate1000 <olenakuznietsova@yandex.ru>
2022-09-28 16:56:02 +0300

    Add driver fixture

--- test/test_header.py
+++ test/test_header.py
@@ -1,46 +1,38 @@
 import allure
-from selenium import webdriver
 
-from page_object import header
+from page_object.header import HeaderPage
 from page_object import yandex_page
-from page_object import main_page
-from page_object import order_page
+from page_object.main_page import MainPage
+from page_object.order_page import OrderPage
 
 
 class TestHeader:
     driver = None
-    url = main_page.MainPage.url
-
-    @classmethod
-    def setup_class(cls):
-        cls.driver = webdriver.Chrome()
+    url = MainPage.url
 
     @allure.title('Проверка работы кнопки логотипа «Самоката»')
     @allure.description('Если нажать на логотип «Самоката», попадёшь на главную страницу «Самоката».')
-    def test_click_on_scooter_logo_open_scooter_page(self):
-        self.driver.maximize_window()
-        self.driver.get(order_page.OrderPage.url)
+    def test_click_on_scooter_logo_open_scooter_page(self, driver):
+        driver.maximize_window()
+        driver.get(OrderPage.url)
 
-        page = header.HeaderPage(self.driver)
+        page = HeaderPage(driver)
         page.click_scooter_logo()
 
-        page = main_page.MainPage(self.driver)
+        page = MainPage(driver)
 
-        assert page.url == self.driver.current_url
+        assert page.url == driver.current_url
 
     @allure.title('Проверка работы кнопки логотипа «Яндекс»')
     @allure.description('Если нажать на логотип «Яндекс», попадёшь на главную страницу «Яндекс».')
-    def test_click_on_yandex_logo_open_yandex_page(self):
-        self.driver.maximize_window()
-        self.driver.get(self.url)
+    def test_click_on_yandex_logo_open_yandex_page(self, driver):
+        driver.maximize_window()
+        driver.get(self.url)
 
-        page = header.HeaderPage(self.driver)
+        page = HeaderPage(driver)
         page.click_yandex_logo()
-        self.driver.switch_to.window(self.driver.window_handles[-1])
-        yandex = yandex_page.YandexPage(self.driver)
+        driver.switch_to.window(driver.window_handles[-1])
+        yandex = yandex_page.YandexPage(driver)
 
-        assert yandex.url == self.driver.current_url
+        assert yandex.url == driver.current_url
 
-    @classmethod
-    def teardown_class(cls):
-        cls.driver.quit()
